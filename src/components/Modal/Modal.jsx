22d67008b627fe4ab88b9381c7870237e25aa757
LisovaOl <lisova.olenka@gmail.com>
2022-10-27 15:22:26 +0300

    modalRoot

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,10 @@
 import { Component } from 'react';
+import { createPortal } from 'react-dom'; // 8-modal подключить портал через метод из react-dom
+
 // import PropTypes from 'prop-types';
 import '../../index.css';
+
+const modalRoot = document.querySelector('#modal-root');
 
 export default class Modal extends Component {
 
@@ -25,15 +29,17 @@ export default class Modal extends Component {
 //   };
 
   render() {
-    return (
+    return createPortal(
       <div className="Overlay" onClick={this.handleBackdropClick}>
         <div className="Modal">
+          {this.props.children}
           <img
             src="https://pixabay.com/get/g32d0b3a115fbff8823764e55ba131db4abf320ace7e0c79e9a82edd76a8124253abe4c2c40bbf44ae55dcbf6b4285a9d5c600468a47791af2f8ae942014bff61_640.jpg"
             alt=""
           ></img>
         </div>
-      </div>
+      </div>,
+      modalRoot
     );
   }
 }
