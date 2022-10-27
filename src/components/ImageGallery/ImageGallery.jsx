e94e618aea05c130b056de01eb303e242d1e0d09
LisovaOl <lisova.olenka@gmail.com>
2022-10-27 14:57:28 +0300

    key={galleryItem.id}

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,26 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../index.css';
 
-// import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
 const ImageGallery = ({ galleryItems }) => {
   return (
-    <div >
+    <div>
       <ul className="ImageGallery">
         {galleryItems.map(galleryItem => {
           return (
-            <li key={galleryItem.id} className="ImageGalleryItem">
-              <img
-                className="ImageGalleryItem-image"
-                src={galleryItem.webformatURL}
-                alt={galleryItem.tags}
-              ></img>
-            </li>
+            <ImageGalleryItem key={galleryItem.id} galleryItem={galleryItem} />
 
-            // <ImageGalleryItem
-            //   key={galleryItem.id}
-            //   galleryItem={galleryItem}
-            // />
           );
         })}
       </ul>
@@ -39,4 +29,3 @@ ImageGallery.propTypes = {
 };
 
 export default ImageGallery;
-
