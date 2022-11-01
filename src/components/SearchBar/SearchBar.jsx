e290e765613da874f25746d230203ea5a090208f
LisovaOl <lisova.olenka@gmail.com>
2022-11-01 19:23:16 +0200

    search-hoooks

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,53 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FcSearch } from 'react-icons/fc';
 import PropTypes from 'prop-types';
 
 import 'react-toastify/dist/ReactToastify.css';
 import '../../index.css';
+  
+export default function SearchBar({onSubmit}) {
+  const [inputQuery, setInputQuery] = useState('');
 
-export class SearchBar extends Component {
-  state = {
-    inputQuery: '',
+  const handleNameChange = event => {
+    setInputQuery(event.currentTarget.value.toLowerCase());
   };
 
-  handleNameChange = event => {
-    this.setState({ inputQuery: event.currentTarget.value.toLowerCase() });
-  };
-
-  render() {
-    return (
-      <div>
-        <header className="Searchbar">
-          <form
-            className="SearchForm"
-            onSubmit={event => {
-              event.preventDefault();
-              this.props.onSubmit(this.state.inputQuery);
-              event.target.reset(); //очистка поля введення
-            }}
-          >
-            <input
-              className="SearchForm-input"
-              type="text"
-              autoComplete="off"
-              autoFocus
-              placeholder="Search images and photos"
-              onChange={this.handleNameChange}
-            />
-            <button type="submit" className="SearchForm-button">
-              {/* <span className="SearchForm-button-label"> */}
-              <FcSearch />
-              {/* </span> */}
-            </button>
-          </form>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <header className="Searchbar">
+        <form
+          className="SearchForm"
+          onSubmit={event => {
+            event.preventDefault();
+            onSubmit(inputQuery);
+            event.target.reset(); //очистка поля введення
+          }}
+        >
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={handleNameChange}
+          />
+          <button type="submit" className="SearchForm-button">
+            {/* <span className="SearchForm-button-label"> */}
+            <FcSearch />
+            {/* </span> */}
+          </button>
+        </form>
+      </header>
+    </div>
+  );
 }
 
-export default SearchBar;
-
 SearchBar.propTypes = {
+  inputQuery: PropTypes.string,
   onSubmit: PropTypes.func,
 };
+
+// export class SearchBar extends Component {
+//   state = {
+//     inputQuery: '',
+//   };
+
+//   handleNameChange = event => {
+//     this.setState({ inputQuery: event.currentTarget.value.toLowerCase() });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <header className="Searchbar">
+//           <form
+//             className="SearchForm"
+//             onSubmit={event => {
+//               event.preventDefault();
+//               this.props.onSubmit(this.state.inputQuery);
+//               event.target.reset(); //очистка поля введення
+//             }}
+//           >
+//             <input
+//               className="SearchForm-input"
+//               type="text"
+//               autoComplete="off"
+//               autoFocus
+//               placeholder="Search images and photos"
+//               onChange={this.handleNameChange}
+//             />
+//             <button type="submit" className="SearchForm-button">
+//               {/* <span className="SearchForm-button-label"> */}
+//               <FcSearch />
+//               {/* </span> */}
+//             </button>
+//           </form>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default SearchBar;
+
